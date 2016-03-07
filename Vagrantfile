36ac910096238e07105800d8110328646f60a105
Paul Canham <paul@canham.eu>
2016-03-07 22:58:35 +0000

    upgraded to work with centos 7

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,10 @@ $script = <<SCRIPT
 echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 puppet module install --force puppetlabs/stdlib
 puppet module install --force puppetlabs/java
+puppet module install --force nanliu/staging
+puppet module install --force camptocamp/archive
 puppet module install --force rtyler/jenkins
+puppet module install --force puppetlabs/firewall
 SCRIPT
 
 
@@ -23,20 +26,25 @@ Vagrant.configure("2") do |config|
 
 
   config.vm.define :jenkins do |jenkins|
-    jenkins.vm.box = "puppet-centos-65-x64"
-    jenkins.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/centos-65-x64-virtualbox-puppet.box"
+    jenkins.vm.box = "puppet-centos-70-x64"
+    jenkins.vm.box = "puppetlabs/centos-7.0-64-puppet"
+    jenkins.vm.box_version = '1.0.1'
+    jenkins.vm.box_check_update = true
     jenkins.vm.network "private_network", ip: "10.0.0.20"
     #jenkins.vm.network "public_network"
     jenkins.vm.hostname = "jenkins"
     jenkins.vm.provider :virtualbox do |v|
-      v.customize ["modifyvm", :id, "--memory", "2048" ]
+      v.customize ["modifyvm", :id, "--memory", "4096" ]
     end
     jenkins.vm.provision :shell, :inline => $script
     jenkins.vm.provision :puppet,
       :options => ["--debug", "--verbose", "--summarize"],
       :facter => { "fqdn" => "jenkins.sandbox.internal" } do |puppet|
-        puppet.manifests_path = "./"
+        puppet.manifests_path = "."
         puppet.manifest_file = "jenkins.pp"
+        puppet.environment = 'all'
+        #puppet.environment_path = 'environments'
+        #puppet.module_path = "/etc/puppetlabs/code/environments/production/modules"
     end
   end
 end
--- jenkins.pp
+++ jenkins.pp
@@ -1,27 +1,40 @@
 node default {
-  class { 'jenkins':
-    plugin_hash => {
-      'git'                   => {},
-      'promoted-builds'       => {},
-      'maven-plugin'          => {},
-      'javadoc'               => {},
-      'token-macro'           => {},
-      'mailer'                => {},
-      'parameterized-trigger' => {},
-      'conditional-buildstep' => {},
-      'subversion'            => {},
-      'mapdb-api'             => {},
-      'bitbucket-oauth'       => {},
-      'ssh-credentials'       => {},
-      'credentials'           => {},
-      'run-condition'         => {},
-      'scm-api'               => {},
-      'multiple-scms'         => {},
-      'git-client'            => {},
-      'rvm'                   => {},
-      'python-wrapper'        => {}
-    }
+  class { 'firewall': }
+  firewall { '100 allow http on port 8080':
+    dport  => [ 8080 ],
+    proto  => tcp,
+    action => accept,
   }
+
+  class { 'jenkins': }
+  jenkins::user { 'johndoe':
+    email    => 'jdoe@example.com',
+    password => 'changeme',
+  }
+
+  jenkins::plugin { 'git': }
+  jenkins::plugin { 'promoted-builds': }
+  jenkins::plugin { 'maven-plugin': }
+  jenkins::plugin { 'javadoc': }
+  jenkins::plugin { 'token-macro': }
+  jenkins::plugin { 'mailer': }
+  jenkins::plugin { 'parameterized-trigger': }
+  jenkins::plugin { 'conditional-buildstep': }
+  jenkins::plugin { 'subversion': }
+  jenkins::plugin { 'mapdb-api': }
+  jenkins::plugin { 'bitbucket-oauth': }
+  jenkins::plugin { 'ssh-credentials': }
+  jenkins::plugin { 'credentials': }
+  jenkins::plugin { 'run-condition': }
+  jenkins::plugin { 'scm-api': }
+  jenkins::plugin { 'multiple-scms': }
+  jenkins::plugin { 'git-client': }
+  jenkins::plugin { 'rvm': }
+  jenkins::plugin { 'python-wrapper': }
+  jenkins::plugin { 'ansible': }
+  jenkins::plugin { 'job-dsl': }
+  jenkins::plugin { 'workflow-step-api': }
+  jenkins::plugin { 'aws-codepipeline': }
 }
 
 
