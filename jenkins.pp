17a5a3bcb087b40c784d47806a13ca41dc714085
Paul Canham <pcanham@cambridge.org>
2016-05-16 09:52:37 +0100

    fixing allow_virtual_packages error

--- jenkins.pp
+++ jenkins.pp
@@ -1,4 +1,15 @@
+Yumrepo <| |> -> Package <| |>
+
+# Death to the allow_virtual_packages warning
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
 node default {
+
   class { 'firewall': }
 
   class { 'jenkins':
